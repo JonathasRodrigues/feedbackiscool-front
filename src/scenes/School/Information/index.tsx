2d8522f0207a905cdbd6dd9735bbe242814465ea
Jonathas <jonathas.rodrigues@eduzz.com>
2019-03-12 16:08:05 +0000

    add nome de usuário

--- src/scenes/School/Information/index.tsx
+++ src/scenes/School/Information/index.tsx
@@ -59,7 +59,7 @@ class Information extends React.Component<IProps, IStates> {
             <Popover placement='bottom' content={content}>
               <Rate defaultValue={item.generalPoints} allowHalf disabled/> <Icon type= 'down-circle'/> <br />
             </Popover>
-            <p>{item.user ? item.user : 'Usuário '}
+            <p>{!item.anonynous ? `${item.user.username} ` : 'Usuário '}
               estudou {item.course === 'GE' ? 'General English ' : 'Preparatório Trinity '}
               em {moment(item.startDate).format('MMMM/YYYY')}</p>
             <div className='is-locked'>
@@ -75,7 +75,7 @@ class Information extends React.Component<IProps, IStates> {
           <Popover placement='bottom' content={content}>
             <Rate defaultValue={item.generalPoints} allowHalf disabled/> <Icon type= 'down-circle'/> <br />
           </Popover>
-          <p>{item.user ? item.user : 'Usuário '}
+          <p>{!item.anonynous ? `${item.user.username} `: 'Usuário '}
               estudou {item.course === 'GE' ? 'General English ' : 'Preparatório Trinity '}
               em {moment(item.startDate).format('MMMM/YYYY')}</p>
           <p> <strong>Prós:</strong> {item.pros}</p>
