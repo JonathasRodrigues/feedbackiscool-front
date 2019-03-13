f6264ab5d12a90b30dc848030ce650210a08b943
Jonathas <jonathas.rodrigues@eduzz.com>
2019-03-13 09:29:23 +0000

    commented registerServiceWorker import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import Routes from 'scenes/routes';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import store from 'store';
 import { LocaleProvider } from 'antd';
 import pt_BR from 'antd/lib/locale-provider/pt_BR';
