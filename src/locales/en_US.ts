7ae643a988987f09830c589a21becae0a75b8cf5
Byttar <gabriel.nadaleti@gmail.com>
2019-03-24 14:51:44 -0300

    Locker translation fix

--- src/locales/en_US.ts
+++ src/locales/en_US.ts
@@ -62,7 +62,7 @@ const messages = {
   whyTitle: 'Why we created Feedback is Cool?',
   whyText: 'When we were searching for cities and schools avaiable to study exchange we realized that more detailed information and feedback about schools were not avaiable. We analised that this was not just a problem that we went throught, there we had a snap and the idea came of gather all information about schools for study exchange in one place to be evaluated by students and ex-students. In that way, is possible to help people who are searching for study exchange and even who is looking foward renewing its course in a diferent school.',
   lockedText1:'To acess this content, ',
-  lockedText2:'Make a feedback of a school or tell us what your looking foward studying and help the comunity.',
+  lockedText2:'Make a feedback of a school or tell us what you are looking foward studying and help the community.',
   lockedTextAuth1: 'Already made a feedback?',
   lockedTextAuth2: 'Enter',
   lockedTextAuth3: 'It is quick and easy'
